9d0f0e79f7b16eca2bb6cc72ff54eaa0c96589b8
laura <l.restrepop@uniandes.edu.co>
2025-09-07 12:47:32 -0500

    Adicion de timestamp

--- experimento/pedidos/test_endpoint.sh
+++ experimento/pedidos/test_endpoint.sh
@@ -1,5 +1,9 @@
-for i in {1..100}; do
+start_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Start time: $start_time"
+for i in {1..1}; do
   echo "Request $i"
   time curl -w "HTTP Response Code: %{http_code}\n" http://localhost:8000/orders
   echo "--"
 done
+end_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "End time: $end_time"
